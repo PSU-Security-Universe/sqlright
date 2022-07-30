d7f8e3e91cd3839918b50e380209043dbc26ab7a
Yu Liang <luy70@psu.edu>
2022-07-30 01:15:57 -0400

    Small update on sqlite setup script. Work correctly with/without sqlite_binary_zip.

--- SQLite/scripts/setup_sqlite.sh
+++ SQLite/scripts/setup_sqlite.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 cd "$(dirname "$0")"/../docker
 
-if [ ! -d ./sqlite_bisecting_binary ]; then
-    if [ -f ./sqlite_bisecting_binary/sqlite_bisecting_binary.zip.001 ]; then
-        cd sqlite_bisecting_binary_zip
-        cat sqlite_bisecting_binary.zip* > ./sqlite_bisecting_binary_zip.zip
-        echo "Unzipping files. Please wait. "
-        unzip sqlite_bisecting_binary_zip.zip &> /dev/null
-        mv sqlite_bisecting_binary ../sqlite_bisecting_binary
-        cd ../
-    fi
+if [ -e "./sqlite_bisecting_binary_zip/sqlite_bisecting_binary.zip.001" ]; then
+    echo "Unzipping files. Please wait. "
+    cd sqlite_bisecting_binary_zip
+    cat sqlite_bisecting_binary.zip* > ./sqlite_bisecting_binary_zip.zip
+    unzip sqlite_bisecting_binary_zip.zip &> /dev/null
+    rm -rf ../sqlite_bisecting_binary
+    mv sqlite_bisecting_binary ../sqlite_bisecting_binary
+    cd ../
+else
+    echo "sqlite_bisecting_binary zip files not existed. Skip SQLite bisecting setup steps. "
 fi
-
 
 ## For debug purpose, keep all intermediate steps to fast reproduce the run results.
 #sudo docker build --rm=false -f ./Dockerfile -t sqlright_sqlite .
